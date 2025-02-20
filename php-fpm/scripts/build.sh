c89d320e4ef96bcaf93d0cf3cdcfdc4141a3e062
Brett Patterson <brett.patterson@classyllama.com>
2025-02-19 23:21:29 -0500

    Fix build / reference errors

--- php-fpm/scripts/build.sh
+++ php-fpm/scripts/build.sh
@@ -3,15 +3,8 @@ set -e
 trap 'error "$(printf "Command \`%s\` at $BASH_SOURCE:$LINENO failed with exit code $?" "$BASH_COMMAND")"' ERR
 
 ## find directory where this script is located following symlinks if neccessary
-readonly BASE_DIR="$(
-  cd "$(
-    dirname "$(
-      (readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}") \
-        | sed -e "s#^../#$(dirname "$(dirname "${BASH_SOURCE[0]}")")/#"
-    )"
-  )" >/dev/null \
-  && pwd
-)/../.."
+## 
+readonly BASE_DIR="$(dirname "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")")"
 pushd ${BASE_DIR} >/dev/null
 
 function version {
@@ -29,15 +22,16 @@ fi
 
 ## since fpm images no longer can be traversed, this script should require a search path vs defaulting to build all
 if [[ -z ${SEARCH_PATH} ]]; then
-  echo "::error title=Search Path Undefined::Missing search path. Please try again passing an image type as an argument."
+  echo "::error title=Search Path Undefined" \
+    "::Missing search path. Please try again passing an image type as an argument."
   exit 1
 fi
 
 ## login to docker hub as needed
 if [[ ${PUSH_FLAG} != 0 && ${PRE_AUTH:-0} != 1 ]]; then
-  if [[ ${DOCKER_USERNAME:-} ]]; then
+  if [[ ${DOCKERHUB_USERNAME:-} ]]; then
     echo "Attempting non-interactive docker login (via provided credentials)"
-    echo "${DOCKER_PASSWORD:-}" | docker login -u "${DOCKER_USERNAME:-}" --password-stdin ${DOCKER_REGISTRY:-docker.io}
+    echo "${DOCKERHUB_TOKEN:-}" | docker login -u "${DOCKERHUB_USERNAME:-}" --password-stdin ${DOCKER_REGISTRY:-docker.io}
   elif [[ -t 1 ]]; then
     echo "Attempting interactive docker login (tty)"
     docker login ${DOCKER_REGISTRY:-docker.io}
@@ -59,18 +53,17 @@ fi
 # export PHP_SOURCE_IMAGE ENV_SOURCE_IMAGE
 
 if [[ -z ${PHP_VERSION} ]]; then
-  echo "::error title=PHP Version Undefined::Building ${SEARCH_PATH} images requires PHP_VERSION env variable be set."
+  echo "::error title=PHP Version Undefined" \
+    "::Building ${SEARCH_PATH} images requires PHP_VERSION env variable be set."
   exit 2
 fi
 
 ## Build Dir should be the search path + variant (e.g. php-fpm/magento2)
-BUILD_DIR="$(dirname ${SEARCH_PATH}/${VARIANT})"
+BUILD_DIR="${SEARCH_PATH}/${VARIANT}"
+
 ## Image name should only be the top-level directory (e.g. php-fpm)
 IMAGE_NAME=$(echo "${BUILD_DIR}" | cut -d/ -f1)
-
-if [[ "${INDEV_FLAG:-1}" != "0" ]]; then
-  IMAGE_NAME="${IMAGE_NAME}-indev"
-fi
+[[ "${INDEV_FLAG:-1}" != "0" ]] && IMAGE_NAME="${IMAGE_NAME}-indev"
 
 ## Tag suffix should be the variant (e.g. magento2, magento2-xdebug3)
 TAG_SUFFIX=$(echo ${VARIANT} | tr / - | sed 's/^-//')
@@ -87,7 +80,8 @@ PUSH_FLAG=""
 
 # Skip build of xdebug3 fpm images on older versions of PHP (it requires PHP 7.2 or greater)
 if [[ ${IMAGE_SUFFIX} =~ xdebug3 ]] && test $(version ${PHP_VERSION}) -lt $(version "7.2"); then
-  echo "::notice file=php-fpm/scripts/build.sh,line=96,title=::Skipping build for ${IMAGE_NAME}:${IMAGE_TAG} (xdebug3 is unavailable for PHP/${PHP_VERSION})"
+  echo "::notice file=php-fpm/scripts/build.sh,line=96,title=Xdebug Unavailable" \
+    "::Skipping build for ${IMAGE_NAME}:${IMAGE_TAG} (xdebug3 is unavailable for PHP/${PHP_VERSION})"
   exit 0
 fi
 
@@ -103,68 +97,81 @@ docker buildx use warden-builder >/dev/null 2>&1 || docker buildx create --name 
 
 echo "::group::Building ${IMAGE_NAME}:${IMAGE_TAG} (${TAG_SUFFIX})"
 
-docker buildx build \
-  --load \
-  --platform=${PLATFORM} \
-  -t "${IMAGE_NAME}:build" \
-  -f ${BUILD_DIR}/Dockerfile \
-  $(printf -- "--build-arg %s " "${BUILD_ARGS[@]}") \
-  "${BUILD_CONTEXT}"
+  echo "Platform ........... : ${PLATFORM}"
+  echo "ENV Source Image ... : ${ENV_SOURCE_IMAGE}" 
+  echo "PHP Source Image ... : ${PHP_SOURCE_IMAGE}" 
+  echo "PHP Version ........ : ${PHP_VERSION}" 
+  echo "PHP Variant ........ : ${PHP_VARIANT}" 
+  echo "Image Name ......... : ${IMAGE_NAME}" 
 
-# Load the image appropriate for the current runner
-#docker buildx build --load -t "${IMAGE_NAME}:build" $(printf -- "--build-arg %s " "${BUILD_ARGS[@]}") "${BUILD_CONTEXT}" 
+  docker buildx build \
+    --load \
+    --platform=${PLATFORM} \
+    -t "${IMAGE_NAME}:build" \
+    -f ${BUILD_DIR}/Dockerfile \
+    $(printf -- "--build-arg %s " "${BUILD_ARGS[@]}") \
+    "${BUILD_CONTEXT}"
 
-# Fetch the precise php version from the built image and tag it
-MINOR_VERSION="$(docker run --rm -t --entrypoint php "${IMAGE_NAME}:build" -r 'echo phpversion();')"
+  # Fetch the precise php version from the built image and tag it
+  MINOR_VERSION="$(docker run --rm -t --entrypoint php "${IMAGE_NAME}:build" -r 'echo phpversion();')"
 
-MAJOR_TAG="${PHP_VERSION}"
-MINOR_TAG="${MINOR_VERSION}"
-## If the suffix is not empty, append it to the image tag
-if [[ -n "${TAG_SUFFIX}" ]]; then
-  MAJOR_TAG="${MAJOR_TAG}-${TAG_SUFFIX}"
-  MINOR_TAG="${MINOR_TAG}-${TAG_SUFFIX}"
-fi
+  MAJOR_TAG="${PHP_VERSION}"
+  MINOR_TAG="${MINOR_VERSION}"
+  ## If the suffix is not empty, append it to the image tag
+  if [[ -n "${TAG_SUFFIX}" ]]; then
+    MAJOR_TAG="${MAJOR_TAG}-${TAG_SUFFIX}"
+    MINOR_TAG="${MINOR_TAG}-${TAG_SUFFIX}"
+  fi
 
-IMAGE_TAGS=(
-  "${IMAGE_NAME}:${MAJOR_TAG}"
-  "${IMAGE_NAME}:${MINOR_TAG}"
-)
+  IMAGE_TAGS=(
+    "${IMAGE_NAME}:${MAJOR_TAG}"
+    "${IMAGE_NAME}:${MINOR_TAG}"
+  )
 
 echo "::endgroup::"
 
 echo "::group::Pushing layers to registries for ${IMAGE_NAME}:${IMAGE_TAG}${TAG_SUFFIX}"
-printf "\e[01;31m==> building ${IMAGE_TAG} from ${BUILD_DIR}/Dockerfile with context ${BUILD_CONTEXT}\033[0m\n"
 
-NAMES=()
-for registry in $(jq -r '.[]' <<< "${REGISTRIES}"); do
-  NAMES+=("${registry}/${IMAGE_NAME}")
-done
+  printf "\e[01;31m==> building ${IMAGE_TAG} from ${BUILD_DIR}/Dockerfile with context ${BUILD_CONTEXT}\033[0m\n"
 
-docker buildx build \
-  $PUSH_FLAG \
-  --platform=${PLATFORM} \
-  --metadata-file metadata.json \
-  --output=type=image,name="$(IFS=, ; echo "${NAMES[*]}")",push-by-digest=true,name-canonical=true \
-  -f ${BUILD_DIR}/Dockerfile \
-  $(printf -- "--build-arg %s " "${BUILD_ARGS[@]}") \
-  "${BUILD_CONTEXT}"
+  NAMES=()
+  for registry in $(jq -r '.[]' <<< "${REGISTRIES}"); do
+    NAMES+=("${registry}/${IMAGE_NAME}")
+  done
+
+  docker buildx build \
+    $PUSH_FLAG \
+    --platform=${PLATFORM} \
+    --metadata-file metadata.json \
+    --output=type=image,name="$(IFS=, ; echo "${NAMES[*]}")",push-by-digest=true,name-canonical=true \
+    -f ${BUILD_DIR}/Dockerfile \
+    $(printf -- "--build-arg %s " "${BUILD_ARGS[@]}") \
+    "${BUILD_CONTEXT}"
 
 echo "::endgroup::"
+
 echo "::group::Metadata for ${IMAGE_NAME}:${IMAGE_TAG}${TAG_SUFFIX}"
 
-cat metadata.json
+  cat metadata.json
 
 echo "::endgroup::"
 
 echo "::group::Compiling and mapping metadata"
 
-tagHash=$(echo "${IMAGE_TAGS[@]}" | sha256sum | awk '{print $1}')
-digest=$(jq -r '."containerimage.digest"' metadata.json)
-tagsJSON=$(printf '%s\n' "${IMAGE_TAGS[@]}" | jq -R . | jq -cs .)
-JSON=$(jq -n --arg imageName "${IMAGE_NAME}" --arg digest "${digest}" --arg hash "${tagHash}" --argjson tags "${tagsJSON}" '{ ($hash): { image: $imageName, digests: [$digest], tags: $tags }}')
+  tagHash=$(echo "${IMAGE_TAGS[@]}" | sha256sum | awk '{print $1}')
+  digest=$(jq -r '."containerimage.digest"' metadata.json)
+  tagsJSON=$(printf '%s\n' "${IMAGE_TAGS[@]}" | jq -R . | jq -cs .)
+  JSON=$(
+    jq -n \
+      --arg imageName "${IMAGE_NAME}" \
+      --arg digest "${digest}" \
+      --arg hash "${tagHash}" \
+      --argjson tags "${tagsJSON}" \
+      '{ ($hash): { image: $imageName, digests: [$digest], tags: $tags }}'
+  )
 
-# Create file placeholders for digests and tags
-mkdir -p "${METADATA_DIR}"
-echo "${JSON}" > "${METADATA_DIR}/${IMAGE_NAME}-${IMAGE_TAG//\//-}${TAG_SUFFIX//\//-}.json"
+  # Create file placeholders for digests and tags
+  mkdir -p "${METADATA_DIR}"
+  echo "${JSON}" > "${METADATA_DIR}/${IMAGE_NAME}-${IMAGE_TAG//\//-}${TAG_SUFFIX//\//-}.json"
 
 echo "::endgroup::"
